a193165f590b421bc2d5901116f122386b739633
Fefe Lemaire <fede.lemaire@gmail.com>
2020-06-05 05:24:52 -0300

    Segues

---
+++ swoosh-app/swoosh-app/Controller/LeagueVC.swift
@@ -0,0 +1,39 @@
+//
+//  LeagueVC.swift
+//  swoosh-app
+//
+//  Created by Fede Lemaire on 6/5/20.
+//  Copyright © 2020 Fede Lemaire. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+        @IBAction func onNextTapped(_ sender: Any) {
+            performSegue(withIdentifier: "skillVCSegue", sender: self)
+           }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swoosh-app/swoosh-app/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  swoosh-app
+//
+//  Created by Fede Lemaire on 6/5/20.
+//  Copyright © 2020 Fede Lemaire. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
